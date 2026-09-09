cae1241fb578f48f53b372044a78d119fef16d9d
Jocie Marino <jociemarino@gmail.com>
2026-09-09 14:43:19 -0400

    Add header comment and introduction lines

--- lab0-hello/HelloWorld/Program.cs
+++ lab0-hello/HelloWorld/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Jocie Marino
+* CSCI 1250, section 002
+* lab 0, hello world
+* september 8, 2026
+* prints a greeting and short introduction to the console
+*/
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Jocie Marino.");
+Console.WriteLine("This is my first program at ETSU.");
